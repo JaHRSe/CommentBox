392492dd0279cbdb280da8297f631d4544ebdec1
Jacob Searles <jacobsearles@Jacobs-MacBook-Pro.local>
2020-01-03 22:05:07 -0500

    collect static

--- commentbox/settings/base.py
+++ commentbox/settings/base.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'commentbox.urls'
@@ -119,14 +119,14 @@ USE_TZ = True
 ###############
 # Static Files
 ###############
-
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'comment/static/'),
-#     os.path.join(BASE_DIR, 'commentbox/static/'),
-# ]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 ###############
 # Email Setup
